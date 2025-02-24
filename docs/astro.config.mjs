796b73915ca6c8e940edb804c6599c4d8580df6f
Tapas Sharma <tapas@platform9.com>
2025-02-24 16:47:23 +0530

    Add github action to deploy
    Signed-off-by: Tapas Sharma <tapas@platform9.com>

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -4,6 +4,8 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://sharma-tapas.github.io',
+	base: 'vjailbreak',
 	integrations: [
 		starlight({
 			title: 'vJailbreak',
